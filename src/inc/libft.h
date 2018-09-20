8063e1ff1285f3194bd6a3d551412d3e388017ab
Yener Tüz <yenertuz@Yeners-iMac.local>
2018-09-19 18:40:27 -0700

    final working version

--- src/inc/libft.h
+++ src/inc/libft.h
@@ -3,7 +3,6 @@
 
 # include <unistd.h>
 # include <stdlib.h>
-# include <fcntl.h>
 
 # define MALLOC(p1, p2) (p1*)malloc(sizeof(p1) * p2);
 # define MEMALLOC(p1, p2) (p1*)ft_memalloc(sizeof(p1) * p2);
@@ -98,8 +97,6 @@ void			ft_lstiter(t_list *s, void (*f)(t_list *e));
 t_list			*ft_lstmap(t_list *s, t_list* (*f)(t_list *e));
 
 int				ft_gnl(int const fd, char **line);
-
-char			*ft_readfile(char const *p);
 
 
 /*
