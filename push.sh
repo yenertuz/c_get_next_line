63d3b8f64df276a1e83615f3e35a455f2c22a5d5
Yener Tüz <yenertuz@Yeners-iMac.local>
2018-09-22 23:43:45 -0700

    m

---
+++ push.sh
@@ -0,0 +1,1 @@
+git clone https://github.com/yenertuz/ft001_gnl.git; rm -rRf ft001_gnl/src ft001_gnl/inc ft001_gnl/libft.a ft001_gnl/*.sh; cp -R src inc *.a *.sh ft001_gnl/; cd ft001_gnl; git add .; git commit -m "m"; git push
